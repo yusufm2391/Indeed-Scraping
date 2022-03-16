d6dc782b13993c87b8a3afc2bb9ca48ea024ab81
YUSUF-NOTEBOOK <yusufm2391@gmail.com>
2022-03-16 07:56:29 +0700

    step to scrape indeed.com

--- step to scrape.py
+++ step to scrape.py
@@ -1,3 +1,5 @@
+#step 9 membuat direktori dengan mengimport lib os
+import os
 
 #step 2 import library
 import requests
@@ -19,9 +21,37 @@ headers = {'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit
 res = requests.get(url, params = params, headers = headers)
 
 #step 7 menggunakan beautifulsoup
-soup=BeautifulSoup(res.text, 'html.parser')
+#soup=BeautifulSoup(res.text, 'html.parser') dicut dipindahkan ke step 10
 
-print(soup.prettify())
+
+
+
+#step 8 membuat fungsi total page yang isinya params dict
+#dihapus aja print(soup.prettify())
+
+def get_total_pages():
+    params = {
+        'q': 'pyhton developer',
+        'l': 'new york'
+    }
+    res = requests.get(url, params = params, headers = headers)
+
+# step 9 lanjutan membuat directory mkdir
+    try:
+        os.mkdir('temp')
+    except FileExistsError:
+        pass
+#step 10 mengextract raw material
+    with open('temp/res.html', 'w+') as outfile:
+        outfile.write(res.text)
+        outfile.close()
+
+#scraping step
+    soup = BeautifulSoup(res.text, 'html.parser')
+    print(soup.prettify())
+
+if __name__ == '__main__':
+    get_total_pages()
 
 #step 4 check status.code sudah selesai
 #print(res.status_code)
